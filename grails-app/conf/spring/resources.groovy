fd625ec88bedbd287100534678ebe523a3705d20
Dev <devinderwadhwa20@gmail.com>
2020-02-01 18:52:23 +0530

    custom auth.gsp spring security by djamware.com

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,7 @@
+import com.mycompany.myapp.UserPasswordEncoderListener
+import com.mycompany.myapp.CustomUserDetailsService
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
+    userDetailsService(CustomUserDetailsService)
 }
--- grails-app/init/grailproj/BootStrap.groovy
+++ grails-app/init/grailproj/BootStrap.groovy
@@ -1,5 +1,9 @@
 package grailproj
 
+import com.mycompany.myapp.Role
+import com.mycompany.myapp.User
+import com.mycompany.myapp.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
@@ -12,6 +16,21 @@ class BootStrap {
             new Book(title: 'book 1 title').save()
 
         }
+
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+        def userRole = new Role(authority: 'ROLE_USER').save()
+
+        def testUser = new User(username: 'tester', password: 'pass').save()
+        def testAdmin = new User(username: 'admin', password: 'password').save()
+
+        UserRole.create testAdmin, adminRole
+        UserRole.create testUser, userRole
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
     }
     def destroy = {
     }
